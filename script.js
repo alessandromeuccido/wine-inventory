aa8548301afcda049efe73c07ed7bb4b071527e1
alessandromeuccido <alessandromeucci9@gmail.com>
2026-05-04 12:55:55 +0200

    Add function ToggleForm / open and close form for adding new wine

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const inventoryWine = [
+const wineBottles = [
   {
-  name:"Pinot Grigio DOC Le Romiglia"
+    name:"Pinot Grigio DOC Le Romiglia",
+    quantity: 2
+  },
+  {
+    name:"Lunario Colli Colli Lanuvini DOP Superiore La Luna Del Casale",
+    quantity: 2
+  }
+  ]
 
-}]
+  function addWine(name, quantity) {
+    wineBottles.push({
+      name: name, 
+      quantity: quantity
+    });
+  };
+
+  function toggleForm() {
+    document.getElementById("add-form").classList.toggle("is-open");
+  }
+
