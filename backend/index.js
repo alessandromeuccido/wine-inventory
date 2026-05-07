77e9b9d5bad647d2293c9836a7e0d4b147ef1dbd
alessandromeuccido <alessandromeucci9@gmail.com>
2026-05-07 11:18:24 +0200

    feat: add API-POST API-DELET API-GET nel database Vini

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,32 @@ app.get('/api/wines', (req, res) => {
   res.json(wines);
 });
 
+// API — POST -> aggiungi un vino
+app.post('/api/wines', (req, res) => {
+
+  // req.body contiene i dati inviati dal frontend
+  const { name, cantina, anno, types, quantity, min, price, status } = req.body;
+
+  const stmt = db.prepare(`
+    INSERT INTO wines (name, cantina, anno, types, quantity, min, price, status)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `);
+
+  const result = stmt.run(name, cantina, anno, types, quantity, min, price, status);
+
+  res.json({ id: result.lastInsertRowid, ...req.body });
+});
+
+// API — DELETE -> elimina un vino
+app.delete('/api/wines/:id', (req, res) => {
+
+  const id = req.params.id;
+
+  db.prepare('DELETE FROM wines WHERE id = ?').run(id);
+
+  res.json({ deleted: true, id: id });
+});
+
 // START A SERVER 
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
