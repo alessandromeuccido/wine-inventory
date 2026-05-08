f439eef3f56e4c489f50ec5fa668378163e0595a
alessandromeuccido <alessandromeucci9@gmail.com>
2026-05-08 10:27:11 +0200

    fix: CORS esplicito per Railway

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  allowedHeaders: ['Content-Type']
+}));
 
 app.use(express.json());
 
